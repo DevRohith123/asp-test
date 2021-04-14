b4184d6d5369d475704caeab70f8bc9ac24aa26f
DevRohith123 <82517079+DevRohith123@users.noreply.github.com>
2021-04-14 18:21:36 +0530

    Initial
    Initial

---
+++ Models/Query.cs
@@ -0,0 +1,18 @@
+﻿using Demo.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class Query
+    {
+        private readonly IEmployee employee;
+        public Query(IEmployee employee)
+        {
+            this.employee = employee;
+        }
+        public IEnumerable<Employee> Employees => employee.GetEmployees();
+    }
+}
